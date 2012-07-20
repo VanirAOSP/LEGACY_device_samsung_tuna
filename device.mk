6ca0dd074f72fa9003349b9e555b722a31e97a5b
sonicxml <sonicxml@gmail.com>
2012-07-19 21:59:04 -0400

    Revert "Enable user builds to capture bugreports via keycombo"
    This reverts commit 9a0cc69665817af7f240ead024c1a359a8aa9160.

--- device.mk
+++ device.mk
@@ -199,10 +199,12 @@ PRODUCT_PACKAGES += \
 	setup_fs
 
 # for bugmailer
-PRODUCT_PACKAGES += send_bug
-PRODUCT_COPY_FILES += \
-	system/extras/bugmailer/bugmailer.sh:system/bin/bugmailer.sh \
-	system/extras/bugmailer/send_bug:system/bin/send_bug
+ifneq ($(TARGET_BUILD_VARIANT),user)
+	PRODUCT_PACKAGES += send_bug
+	PRODUCT_COPY_FILES += \
+		system/extras/bugmailer/bugmailer.sh:system/bin/bugmailer.sh \
+		system/extras/bugmailer/send_bug:system/bin/send_bug
+endif
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
