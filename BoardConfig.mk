405dc17bef266666217159d0bcceabce7a315864
nuclearmistake <nuclearmistake@gmail.com>
2013-03-14 19:48:54 -0400

    D32 >>> D16 for tuna, and for the shorties
    Revert "D16 > D32 for tuna"
    
    This reverts commit 72262f32f5244247e4e6d17cdaed8a59d2523fff.
    
    Change-Id: Ifa0a7ac53cb411cf839badc177113b27f20cae5d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,6 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a9
 ARCH_ARM_HAVE_TLS_REGISTER := true
-ARCH_ARM_USE_D16  := true
 
 TARGET_NO_BOOTLOADER := true
 
@@ -101,3 +100,4 @@ BOARD_LIB_DUMPSTATE := libdumpstate.tuna
 
 BOARD_USES_SECURE_SERVICES := true
 
+TARGET_EXTRA_CFLAGS := $(call cc-ifversion, -ge, 46, $(call cc-option,-mtune=cortex-a9,$(call cc-option,-mtune=cortex-a8)) $(call cc-option,-mcpu=cortex-a9,$(call cc-option,-mcpu=cortex-a8)))
