03dbb2bf1c27b5af932ca6b5aa8997bcd8c4321c
Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>
2013-11-05 15:23:02 +0100

    Galaxy Nexus: Add KitKat support for the sensor drivers
    Change-Id: I5da2e6107a5990a4db172302a03938a11be5d1da
    Signed-off-by: Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>

--- libsensors/Android.mk
+++ libsensors/Android.mk
@@ -26,7 +26,10 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_CFLAGS := -DLOG_TAG=\"Sensors\"
+# For Android versions < 4.4
 LOCAL_C_INCLUDES += hardware/invensense/libsensors
+# For Android versions >= 4.4
+LOCAL_C_INCLUDES += hardware/invensense/60xx/libsensors
 LOCAL_SRC_FILES := \
 	sensors.cpp \
 	InputEventReader.cpp \
